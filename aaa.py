c66980855648dbe649362a6cf77b33f6fecb1efd
Marksman <77986440+Markmsan@users.noreply.github.com>
2024-11-20 12:08:45 +0100

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -15,7 +15,7 @@ default_args = {
 
 # Define the DAG, its parameters, and schedule
 dag = DAG(
-    'git_dag',
+    'git_daga',
     default_args=default_args,
     description='A simple example DAG',
     schedule_interval=timedelta(days=1),
